2ea1c942281e1508871d559808d89f725cdf29e1
daniap14 <xboxx105@gmail.com>
2021-09-28 10:52:51 +0200

    Tema2

---
+++ Tema 2/Assets/Scripts/MiFirstScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiFirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+// (Comentario) (Control + S para guardar)
